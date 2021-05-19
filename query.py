e681b0804a33877e424d04161ae5824576353e01
boredtylin <tylin20@fudan.edu.cn>
2021-05-19 18:27:13 +0800

    Update query.py

--- query.py
+++ query.py
@@ -12,14 +12,17 @@ query = f"http://export.arxiv.org/api/query?search_query=all:{config['subject']}
 data = urllib.request.urlopen(query).read().decode('utf-8')
 
 # parse fetched results
-attr_mapping = {'id': 'link', 'name': 'authors'}
+attr_mapping = {'id': 'link', 'name': 'authors', 'title': 'title'}
 map_attr = lambda x: attr_mapping[x] if x in attr_mapping else x
 
 def convert_field(entry):
   new_entry = entry.copy()
   if 'link' in entry:
     link = entry['link']
-    new_entry['title'] = f'<a href="{link}" target="_blank">{entry["title"]}</a>'
+    title = entry["title"]
+    for highlight in config['highlights']:
+      title = title.replace(highlight, f'<font color="#d00000"><b>{highlight}</b></font>')
+    new_entry['title'] = f'{entry["title"]} (<a href="{link}" target="_blank">link</a>)'
     new_entry.pop('link')
   return new_entry
 
