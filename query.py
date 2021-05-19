8255a873fd75e56922d361cb5ffb8c16f4047f38
boredtylin <tylin20@fudan.edu.cn>
2021-05-19 14:46:04 +0800

    Update query.py

--- query.py
+++ query.py
@@ -5,25 +5,33 @@ import json
 from xml.dom import minidom
 from json2html import *
 
+
 # query from arxiv, using offical query API: https://arxiv.org/help/api/user-manua
 config = json.load(open('config.json'))
 query = f"http://export.arxiv.org/api/query?search_query=all:{config['subject']}&id_list=&start=0&sortBy=lastUpdatedDate&sortOrder=descending&max_results=100"
 data = urllib.request.urlopen(query).read().decode('utf-8')
 
 # parse fetched results
+attr_mapping = {'id': 'link', 'name': 'authors'}
+map_attr = lambda x: attr_mapping[x] if x in attr_mapping else x
+
+def convert_field(entry):
+  if link in entry:
+    id = entry[link]
+    entry[link] = f'<a href="{id}" target="_blank">{id.split("/")[-1]}</a>'
 entries = []
 dom = minidom.parseString(data)
 for entry_obj in dom.getElementsByTagName('entry'):
   entry = {}
   for field in config['fields']:
     property_list = entry_obj.getElementsByTagName(field)
-    entry[field] = ', '.join(map(lambda x: x.childNodes[0].nodeValue, property_list))
+    entry[map_attr(field)] = ', '.join(map(lambda x: x.childNodes[0].nodeValue, property_list))
     entry['update'] = '*' if entry_obj.getElementsByTagName('updated')[0].childNodes[0].nodeValue != \
       entry_obj.getElementsByTagName('published')[0].childNodes[0].nodeValue else ' '
     entries.append(entry)
  
 # print(entries)
-table = json2html.convert(json=entries)
+table = json2html.convert(json=entries, )
 print(table)
 
 with open('./arxiv.html', 'w') as f:
