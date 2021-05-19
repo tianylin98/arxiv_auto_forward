3de1b944b2349d8ff40ce96668ee7e2ba3a762be
boredtylin <tylin20@fudan.edu.cn>
2021-05-19 14:49:03 +0800

    Update query.py

--- query.py
+++ query.py
@@ -8,7 +8,7 @@ from json2html import *
 
 # query from arxiv, using offical query API: https://arxiv.org/help/api/user-manua
 config = json.load(open('config.json'))
-query = f"http://export.arxiv.org/api/query?search_query=all:{config['subject']}&id_list=&start=0&sortBy=lastUpdatedDate&sortOrder=descending&max_results=100"
+query = f"http://export.arxiv.org/api/query?search_query=all:{config['subject']}&id_list=&start=0&sortBy=lastUpdatedDate&sortOrder=descending&max_results=20"
 data = urllib.request.urlopen(query).read().decode('utf-8')
 
 # parse fetched results
@@ -28,9 +28,9 @@ for entry_obj in dom.getElementsByTagName('entry'):
   for field in config['fields']:
     property_list = entry_obj.getElementsByTagName(field)
     entry[map_attr(field)] = ', '.join(map(lambda x: x.childNodes[0].nodeValue, property_list))
-    entry['update'] = '*' if entry_obj.getElementsByTagName('updated')[0].childNodes[0].nodeValue != \
+  entry['update'] = '*' if entry_obj.getElementsByTagName('updated')[0].childNodes[0].nodeValue != \
       entry_obj.getElementsByTagName('published')[0].childNodes[0].nodeValue else ' '
-    entries.append(convert_field(entry))
+  entries.append(convert_field(entry))
  
 # print(entries)
 table = json2html.convert(json=entries, table_attributes="class=\"table table-bordered table-hover\"")
